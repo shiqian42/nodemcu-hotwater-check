7449aa85f87af4b952b782ecbe77c1ad4cb78d43
Arthur Wu <shiqian42@users.noreply.github.com>
2020-06-30 00:16:28 +0800

    init origin code

---
+++ North_Version.lua
@@ -0,0 +1,77 @@
+-- North Version --
+-- author:shiqian42
+-- Define --
+STATUS=0
+LAST_STATUS=1
+-- PIN is D1 on Board--
+HOT_WATER_PIN=1
+
+-- Set GPIO Mode INPUT --
+gpio.mode(HOT_WATER_PIN, gpio.INPUT)
+
+-- WiFI Config --
+station_cfg={}
+station_cfg.ssid="fanruan"
+station_cfg.pwd="fanruan234567"
+
+-- Setup Connection --
+wifi.setmode(wifi.STATION)
+wifi.sta.autoconnect(1)
+wifi.sta.config(station_cfg)
+tmr.create():alarm(1000,tmr.ALARM_AUTO,function(wifi_timer)
+    if wifi.sta.getip() == nil then
+        print("Connecting to AP...\n")
+    else
+        print("Connect OK!")
+        wifi_timer:unregister()
+    end
+end)
+
+print("############ STAGE2 ##############")
+
+-- Check Every 500ms
+tmr.create():alarm(500,tmr.ALARM_AUTO,function(wifi_timer)
+    LAST_STATUS = STATUS
+    STATUS = gpio.read(HOT_WATER_PIN)
+    
+    if STATUS == LAST_STATUS then
+        print("############ Not Change! ##############")
+    elseif STATUS == 1 then
+        HotWaterReady()
+        print("############ HotWaterReady! ###########")
+    elseif STATUS == 0 then
+        HotWaterNotReady()
+        print("############ HotWaterNotReady! ########")
+    end
+end)
+
+-- Messages Sending Methods
+-- Ready Method
+function HotWaterReady()
+    print("HotWaterReady()")
+    http.post('https://tomcat.zyee.me/happy/api/water/status',
+       'Content-Type: application/json\r\n',
+       '{"key": "4F-NORTH","status": "FREE"}',
+       function(code, data)
+        if (code < 0) then
+            print("HTTP request failed")
+        else
+            print(code, data)
+        end
+    end)
+end
+    
+-- Not Ready Method
+function HotWaterNotReady()
+    print("HotWaterNotReady()")
+    http.post('https://tomcat.zyee.me/happy/api/water/status',
+        'Content-Type: application/json\r\n',
+        '{"key": "4F-NORTH","status": "BUSY"}',
+        function(code, data)
+         if (code < 0) then
+             print("HTTP request failed")
+         else
+             print(code, data)
+         end
+        end)
+end
---
+++ ToiletMonitor.lua
@@ -0,0 +1,88 @@
+-- FineToilet --
+-- Toilet Monitor --
+-- author:shiqian42
+-- updated: 2020/06/24 17:18 --
+
+-- Define --
+DOOR_STATUS = 0
+DOOR_PIN = 1
+MONITOR_RSSI = 0
+MAC_ADD = nil
+CONTENT_TYPE = 'Content-Type: application/json\r\n'
+PUBLISH_URL_API = ''
+
+URL_API = 'https://tomcat.zyee.me/happy/api/water/status'
+
+
+-- Set GPIO Mode INPUT --
+gpio.mode(DOOR_PIN, gpio.INPUT)
+
+-- WiFI Configuration --
+station_cfg={}
+station_cfg.ssid="fanruan"
+station_cfg.pwd="fanruan234567"
+
+-- Get Mac as unique Device Code--
+getMac()
+
+-- -- --
+
+-- Methods --
+-- Get Rssi --
+function getRSSI()
+    MONITOR_RSSI = wifi.getrssi()
+end
+
+-- Get MAC Address --
+function getMac()
+    MAC_ADD = wifi.sta.getmac()
+end
+
+-- Wifi Connect --
+function setupWifi()
+    wifi.setmode(wifi.STATION)
+    wifi.sta.autoconnect(1)
+    wifi.sta.config(station_cfg)
+    tmr.create():alarm(1000,tmr.ALARM_AUTO,function(wifi_timer)
+        if wifi.sta.getip() == nil then
+            print("Connecting to AP...\n")
+        else
+            print("Connect OK!")
+            wifi_timer:unregister()
+        end
+    end)
+end
+
+-- Register Service --
+-- Use MAC as device code, register a service. Register every time after reboot.
+function registerService(deviceCode)
+
+end
+
+-- Message Deliver Service --
+-- {"key": "4F-NORTH","status": "FREE"} --
+-- No need to accomplish JSON support, so u need to encode urself. --
+function deliverMessage(message)
+    print(message)
+    http.post(URL_API,CONTENT_TYPE,
+    message,
+    function(code, data)
+     if (code < 0) then
+         print("HTTP request failed")
+     else
+         print(code, data)
+     end
+    end)
+end
+
+-- Battery Test --
+
+
+-- HeartBeat --
+-- Send HeartBeat & Battery Condition --
+
+
+
+
+
+
